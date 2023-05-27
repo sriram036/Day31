7e0fa5dc5df98d0a4cced77cd8f138cccb9a0799
Ravi@036# <admin@admins-MacBook-Pro.local>
2023-05-27 19:58:35 +0530

    Added: Create Database

--- src/main/java/com/bridgelabz/AddressBookService.java
+++ src/main/java/com/bridgelabz/AddressBookService.java
@@ -1,14 +1,18 @@
 package com.bridgelabz;
 
 import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class AddressBookService {
 
     public static void main(String[] args) {
-        Connection conn = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll", "root", "Ravi@007");
+            Statement stmt = con.createStatement();
+            boolean response = stmt.execute("create database address_book_service");
+            System.out.println(response);
         }catch (Exception e) {
             System.out.println(e);
         }
