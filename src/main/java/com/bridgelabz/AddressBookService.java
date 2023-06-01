61b14eb0a505b185e0a34b320bafbed08c765467
Ravi@036# <admin@admins-MacBook-Pro.local>
2023-06-01 21:54:30 +0530

    Added: edit contact

--- src/main/java/com/bridgelabz/AddressBookService.java
+++ src/main/java/com/bridgelabz/AddressBookService.java
@@ -11,8 +11,7 @@ public class AddressBookService {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/address_book_service", "root", "Ravi@007");
             Statement stmt = con.createStatement();
-            String sql = "insert into address_book (first_name, last_name, city, state, zipcode, mobile_number, email) " +
-                    "values ('Sriram', 'Ravichandran', 'Bangalore', 'Karnataka', 123456, 1234567890, 'sr@gmail.com')";
+            String sql = "update address_book set first_name = 'Preethi' where contact_id = 1";
             boolean response = stmt.execute(sql);
             System.out.println(response);
         }catch (Exception e) {
